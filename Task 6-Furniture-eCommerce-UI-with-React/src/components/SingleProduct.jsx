ca4258754d6af206a34d79c3cd8a78c1b47e509f
bug2007 <moumitah2007@gmail.com>
2026-06-03 00:42:50 +0600

    updated SingleProduct component

--- Task 6-Furniture-eCommerce-UI-with-React/src/components/SingleProduct.jsx
+++ Task 6-Furniture-eCommerce-UI-with-React/src/components/SingleProduct.jsx
@@ -1,12 +1,33 @@
 import { useSelector } from "react-redux";
+import arrowImg from '../assets/arrow.png';
 
 export default function SingleProduct() {
     const product = useSelector(state => state.component.selectedProduct)
+    const listedImages = [];
+    let key = 0;
+    for (let i=0; i < 4; i++){
+        listedImages.push(<img key={key} src={product.imgSrc} alt={product.title} />)
+        key++;
+    }
+
     return (
-        <>
-        <img src={product.imgSrc} />
-        {product.title}
-        </>
+        <div className="singleProduct">
+            <div className="singleProduct-direction">
+                <span>Home</span>
+                <img src={arrowImg} alt="Arrow Image" />
+                <span>Shop</span>
+                <img src={arrowImg} alt="Arrow Image" />
+                <span className="vr"></span>
+                <span>{product.title}</span>
+            </div>
+            <div className="singleProduct-details">
+                <span className="listedImages">
+                    {listedImages}
+                </span>
+                <img className="img-lg" src={product.imgSrc} alt={product.title} />
+                
+            </div>
+        </div>
     )
 }
 
